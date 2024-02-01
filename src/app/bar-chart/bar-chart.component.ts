6dcf46b06d786939fbfd7aff5584ccbeff6c66a7
sarahmdht <sarahmdht@gmail.com>
2024-02-01 19:18:55 +0200

    mi progreso

---
+++ src/app/bar-chart/bar-chart.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+
+
+
+@Component({
+  selector: 'app-bar-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './bar-chart.component.html',
+  styleUrl: './bar-chart.component.css'
+})
+export class BarChartComponent {
+}
--- src/app/miperfile/miperfile.component.ts
+++ src/app/miperfile/miperfile.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { Top20SliderComponent } from '../top-20-slider/top-20-slider.component';
+import { BarChartComponent } from '../bar-chart/bar-chart.component';
 
 @Component({
   selector: 'app-miperfile',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, Top20SliderComponent],
+  imports: [HeaderComponent, SidebarComponent, Top20SliderComponent, BarChartComponent],
   templateUrl: './miperfile.component.html',
   styleUrl: './miperfile.component.css'
 })
