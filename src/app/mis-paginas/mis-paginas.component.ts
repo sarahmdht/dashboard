52d8c309d0e50f3043b9cdbf7ea64c5798d16322
sarahmdht <sarahmdht@gmail.com>
2024-02-03 22:59:10 +0200

    working in mi paginas

--- src/app/mis-paginas/mis-paginas.component.ts
+++ src/app/mis-paginas/mis-paginas.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mis-paginas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mis-paginas.component.html',
   styleUrl: './mis-paginas.component.css'
 })
 export class MisPaginasComponent {
+  constructor(){
+  
+  }
+  isCollapsed = true;
 
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
